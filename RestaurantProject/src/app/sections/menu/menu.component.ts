e6e7ce7e45c171b6930770fddf167bcc54934d9b
Pasarica Diana <pasaricadiana@yahoo.com>
2021-07-13 16:57:29 +0300

    Menu component

--- RestaurantProject/src/app/sections/menu/menu.component.ts
+++ RestaurantProject/src/app/sections/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/Service/data.service';
 
 @Component({
   selector: 'app-menu',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-
-  constructor() { }
+  url: string = 'https://localhost:44366/api/Category';
+  itemsUrl: string = 'https://localhost:44366/api/Restaurant/ByCategory/'
+  response: any;
+  items: any
+  constructor(private dataServie: DataService) { }
 
   ngOnInit() {
+    this.getCategories();
+    this.GetMenuItems('1')
   }
-
+  async getCategories() {
+    await this.dataServie.GetCategories(this.url).toPromise().then(
+      (data) => {
+        this.response = data;
+        console.log(this.response)
+      })
+  }
+  GetMenuItems(categoryId: string) {
+    // alert(categoryId)
+    this.dataServie.GetMenuItemsById(this.itemsUrl + categoryId).toPromise().then(
+      res => {
+        this.items = res;
+        console.log(this.items)
+      }
+    )
+  }
 }
